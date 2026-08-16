1be3ffb1437e8a818b28bc5903ee29d46a98c880
ntudor11 <tudorrr.nastasa@gmail.com>
2026-08-16 15:09:45 +0200

    init basic express api in server directory and root package json file with workspace wiring

---
+++ server/app.ts
@@ -0,0 +1,11 @@
+import express, { type Express, type Request, type Response } from "express";
+
+const app: Express = express();
+
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
